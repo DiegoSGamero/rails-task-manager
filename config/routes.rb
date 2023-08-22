0f08ad2b201ff0b3a86e8c3cb465b3b191d4f072
diego joao <diegosgjoao@gmail.com>
2023-08-22 19:00:45 -0300

    task manager sem terminar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  resources :tasks
 end
+
+  # get '/restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post '/restaurants', to: 'restaurants#create'
+
+  # get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch '/restaurants/:id', to: 'restaurants#update'
+
+  # delete '/restaurants/:id', to: 'restaurants#destroy'
